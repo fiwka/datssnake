f4008c0f063feb399403a2c8fda905317389a539
fiwka <dropinclass1@gmail.com>
2024-12-20 23:11:41 +0500

    initial commit

---
+++ src/main/java/xyz/fiwka/datssnake2/Datssnake2Application.java
@@ -0,0 +1,13 @@
+package xyz.fiwka.datssnake2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datssnake2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Datssnake2Application.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/fiwka/datssnake2/Datssnake2ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.fiwka.datssnake2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Datssnake2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
