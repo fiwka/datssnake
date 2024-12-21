7128e6dc5095c3542cb1d6f12f3ca54785481bb5
jEUGENEdev <genyamateyuk@gmail.com>
2024-12-21 14:45:16 +0300

    some updates

--- src/main/java/xyz/fiwka/datssnake2/Test.java
+++ src/main/java/xyz/fiwka/datssnake2/Test.java
@@ -5,11 +5,14 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import xyz.fiwka.datssnake2.feign.DatsSnake2FeignClient;
+import xyz.fiwka.datssnake2.feign.message.GameStateResponse;
 import xyz.fiwka.datssnake2.feign.message.SnakesMoveRequest;
 import xyz.fiwka.datssnake2.feign.message.dto.SnakeDto;
 import xyz.fiwka.datssnake2.model.Direction3D;
 import xyz.fiwka.datssnake2.model.Snake;
+import xyz.fiwka.datssnake2.model.SnakeStatus;
 
+import javax.swing.*;
 import java.util.List;
 
 /**
@@ -26,13 +29,16 @@ public class Test implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        List<SnakeDto> snakes = datsSnake2FeignClient.getGameState("ccf5f1fe-e82f-46c5-b592-e240d66ab2aa").getSnakes();
+        GameStateResponse gameStateResponse = datsSnake2FeignClient.getGameState("ccf5f1fe-e82f-46c5-b592-e240d66ab2aa");
+        List<SnakeDto> snakes = gameStateResponse.getSnakes();
         SnakesMoveRequest snakesMoveRequest = new SnakesMoveRequest();
-        snakes.forEach(snake -> snakesMoveRequest.addMovableSnake(new Snake(snake.getId()), Direction3D.positiveXDirection()));
+        snakes.forEach(snake -> snakesMoveRequest.addMovableSnake(new Snake(snake.getId()), Direction3D.Cords.Y.negative()));
         datsSnake2FeignClient.moveSnake(snakesMoveRequest, "ccf5f1fe-e82f-46c5-b592-e240d66ab2aa");
 
-        Thread.sleep(4000);
+        Thread.sleep(1000);
 
-        System.out.println(datsSnake2FeignClient.getGameState("ccf5f1fe-e82f-46c5-b592-e240d66ab2aa").getSnakes());
+        System.out.println(gameStateResponse.getPoints());
+        System.out.println(datsSnake2FeignClient.getGameState("ccf5f1fe-e82f-46c5-b592-e240d66ab2aa").getSnakes().stream().filter(snake -> snake.getStatus() == SnakeStatus.ALIVE).toList());
+        System.out.println();
     }
 }
