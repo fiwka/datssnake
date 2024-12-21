515d77d0ce2411cb64e39767dbccdcce47cffcd1
Daniil Korchagin <danlia2000789@gmail.com>
2024-12-21 15:14:26 +0300

    правки

--- src/main/java/xyz/fiwka/datssnake2/model/Point3D.java
+++ src/main/java/xyz/fiwka/datssnake2/model/Point3D.java
@@ -1,4 +1,13 @@
 package xyz.fiwka.datssnake2.model;
 
+import java.util.List;
+
 public record Point3D(int x, int y, int z) {
+    public List<Integer> toList() {
+        return List.of(x, y, z);
+    }
+
+    public static Point3D fromList(List<Integer> list) {
+        return new Point3D(list.get(0), list.get(1), list.get(2));
+    }
 }
